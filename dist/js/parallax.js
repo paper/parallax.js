85aae96ed754eb4d6cd927b650470a7ee759b2bf
hahnzhu <hahn.chu91@gmail.com>
2014-09-28 20:22:33 +0800

    degrade for animation-timing-function and fix bug

--- dist/js/parallax.js
+++ dist/js/parallax.js
@@ -1,7 +1,7 @@
 /**
  * @author  : hahnzhu
- * @version : 0.2
- * @date    : 2014-09-17
+ * @version : 0.2.1
+ * @date    : 2014-09-28
  * @repository: https://github.com/hahnzhu/parallax.js
  */
 
@@ -9,22 +9,22 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
 
 !function($) {
 
-    'use strict';
+//  'use strict';
 
     var startPos,           // 开始触摸点(X/Y坐标)
         endPos,             // 结束触摸点(X/Y坐标)
         stage,              // 用于标识 onStart/onMove/onEnd 流程的第几阶段，解决 onEnd 重复调用
         offset,             // 偏移距离
-        direction,          // 翻页方向
+        direction,			// 翻页方向
 
-        curPage,            // page 当前页
+        curPage, 			// page 当前页
         pageCount,          // page 数量
         pageWidth,          // page 宽度
         pageHeight,         // page 高度
 
         $pages,             // page 外部 wrapper
         $pageArr,           // page 列表
-        $animateDom,        // 所有设置 [data-animate] 的动画元素
+        $animateDom,		// 所有设置 [data-animate] 的动画元素
 
         options,            // 最终配置项
 
@@ -60,37 +60,37 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
         indicator: false,       // 是否要有指示导航
         arrow: false,           // 是否要有箭头
         onchange: function(){}, // 回调函数
-        orientationchange: function(){} // 屏幕翻转
+        orientationchange: function(){}	// 屏幕翻转
 
     };
 
 
 
     function init() {
-        
-        // 优先显示加载图
-        if (options.loading) {
-            $('.wrapper').append('<div class="parallax-loading"><i class="ui-loading ui-loading-white"></i></div>');
+    	
+    	// 优先显示加载图
+    	if (options.loading) {
+			$('.wrapper').append('<div class="parallax-loading"><i class="ui-loading ui-loading-white"></i></div>');
         } else {
-            // 允许触摸滑动
+        	// 允许触摸滑动
             movePrevent = false;
         }
 
-        curPage     = 0;
-        direction   = 'stay';
-        pageCount   = $pageArr.length;              // 获取 page 数量
-        pageWidth   = $(window).width();            // 获取手机屏幕宽度
-        pageHeight  = $(window).height();           // 获取手机屏幕高度
-        $animateDom = $('[data-animation]');        // 获取动画元素节点
+		curPage 	= 0;
+		direction	= 'stay';
+        pageCount   = $pageArr.length;           	// 获取 page 数量
+        pageWidth   = $(window).width();         	// 获取手机屏幕宽度
+        pageHeight  = $(window).height();       	// 获取手机屏幕高度
+        $animateDom = $('[data-animation]');	 	// 获取动画元素节点
 
         for (var i=0; i<pageCount; i++) {          // 批量添加 data-id
             $($pageArr[i]).attr('data-id', i+1);
         }
 
-        $pages.addClass(options.direction)      // 添加 direction 类
-                .addClass(options.swipeAnim);   // 添加 swipeAnim 类
+        $pages.addClass(options.direction)		// 添加 direction 类
+            	.addClass(options.swipeAnim);  	// 添加 swipeAnim 类
 
-        $pageArr.css({                          // 初始化 page 宽高
+        $pageArr.css({                    		// 初始化 page 宽高
             'width': pageWidth + 'px',
             'height': pageHeight + 'px'
         });
@@ -100,7 +100,7 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
             $pages.css('height', pageHeight * $pageArr.length);
 
 
-        if (options.swipeAnim === 'cover') {        // 重置 page 的宽高(因为这两个效果与 default 实现方式截然不同)
+        if (options.swipeAnim === 'cover') {		// 重置 page 的宽高(因为这两个效果与 default 实现方式截然不同)
             $pages.css({
                 'width': pageWidth,
                 'height': pageHeight
@@ -109,7 +109,7 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
         }
 
 
-        if (!options.loading) {
+		if (!options.loading) {
             $($pageArr[curPage]).addClass('current');
             options.onchange(curPage, $pageArr[curPage], direction);
             animShow();
@@ -131,7 +131,7 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
             return false;
         }
         
-        touchDown = true;   // 手指已按下
+        touchDown = true;	// 手指已按下
 
         options.direction === 'horizontal' ? startPos = e.pageX : startPos = e.pageY;
 
@@ -249,7 +249,7 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
             }
 
 
-            if (options.indicator) {
+			if (options.indicator) {
                 $($('.parallax-h-indicator ul li, .parallax-v-indicator ul li').removeClass('current').get(curPage)).addClass('current');
             }
             stage = 3;
@@ -445,31 +445,37 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
                 $delay      = $element.attr('data-delay') ?  $element.attr('data-delay') : 0;
 
 
-            if ($animation === 'followSlide') {
-                
-                if (options.direction === 'horizontal' && direction === 'forward') {
-                    $animation = 'followSlideToLeft';
-                }
-                else if (options.direction === 'horizontal' && direction === 'backward') {
-                    $animation = 'followSlideToRight';
-                }
-                else if (options.direction === 'vertical' && direction === 'forward') {
-                    $animation = 'followSlideToTop';
-                }
-                else if (options.direction === 'vertical' && direction === 'backward') {
-                    $animation = 'followSlideToBottom';
-                }
-                
-            }
+			if ($animation === 'followSlide') {
+				
+				if (options.direction === 'horizontal' && direction === 'forward') {
+					$animation = 'followSlideToLeft';
+				}
+				else if (options.direction === 'horizontal' && direction === 'backward') {
+					$animation = 'followSlideToRight';
+				}
+				else if (options.direction === 'vertical' && direction === 'forward') {
+					$animation = 'followSlideToTop';
+				}
+				else if (options.direction === 'vertical' && direction === 'backward') {
+					$animation = 'followSlideToBottom';
+				}
+				
+			}
 
             $element.css({
-                '-webkit-animation': $animation +' '+ $duration + 'ms ' + $timfunc + ' '+ $delay + 'ms both',
-                'animation': $animation +' '+ $duration + 'ms ' + $timfunc + ' '+ $delay + 'ms both'
+//              '-webkit-animation': $animation +' '+ $duration + 'ms ' + $timfunc + ' '+ $delay + 'ms both',
+				
+				// 为了兼容不支持贝塞尔曲线的动画，需要拆开写
+				// 严格模式下不允许出现两个同名属性，所以不得已去掉 'use strict'
+				'-webkit-animation-name': $animation,
+				'-webkit-animation-duration': $duration + 'ms',
+				'-webkit-animation-timing-function': 'ease',
+				'-webkit-animation-timing-function': $timfunc,
+				'-webkit-animation-delay': $delay + 'ms',
+				'-webkit-animation-fill-mode': 'both'
             })
         });
     }
-
-
 
 
 
@@ -488,22 +494,22 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
         })
         .on('webkitAnimationEnd webkitTransitionEnd', '.pages', function() {
 
-            if (direction !== 'stay') {
-                setTimeout(function() {
-                    $(".back").hide().removeClass("back");
-                    $(".front").show().removeClass("front");
-                    $pages.removeClass('forward backward animate');
-                }, 10);
-    
-                $($pageArr.removeClass('current').get(curPage)).addClass('current');
-                options.onchange(curPage, $pageArr[curPage], direction);  // 执行回调函数
-                animShow();
-            }
+			if (direction !== 'stay') {
+				setTimeout(function() {
+	                $(".back").hide().removeClass("back");
+	                $(".front").show().removeClass("front");
+	                $pages.removeClass('forward backward animate');
+	            }, 10);
+	
+	            $($pageArr.removeClass('current').get(curPage)).addClass('current');
+	            options.onchange(curPage, $pageArr[curPage], direction);  // 执行回调函数
+	            animShow();
+			}
             
         });
 
-    
-    $('.page *').on('webkitAnimationEnd', function() {
+	
+	$('.page *').on('webkitAnimationEnd', function() {
         event.stopPropagation();    // 事件代理无法阻止冒泡，所以要绑定取消
     })
 
@@ -524,16 +530,16 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
 
         if (options.indicator) {
             movePrevent = false;
-            
-            var temp = options.direction === 'horizontal' ? 'parallax-h-indicator' : 'parallax-v-indicator'; 
+			
+			var temp = options.direction === 'horizontal' ? 'parallax-h-indicator' : 'parallax-v-indicator'; 
 
             $('.wrapper').append('<div class='+temp+'></div>');
             var lists = '<ul>';
             for (var i=1; i<=pageCount; i++) {
                 if (i===1) {
-                    lists += '<li class="current">'+i+'</li>'
+                    lists += '<li class="current"></li>'
                 } else {
-                    lists += '<li>'+i+'</li>'
+                    lists += '<li></li>'
                 }
             }
             lists += '</ul>';
@@ -553,12 +559,12 @@ if (typeof Zepto === 'undefined') { throw new Error('Parallax.js\'s script requi
     // ============================== 
     
     window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
-        if (window.orientation === 180 || window.orientation === 0) {  
-            options.orientationchange('portrait');
-        }  
-        if (window.orientation === 90 || window.orientation === -90 ){  
-            options.orientationchange('landscape') 
-        }   
+    	if (window.orientation === 180 || window.orientation === 0) {  
+			options.orientationchange('portrait');
+		}  
+		if (window.orientation === 90 || window.orientation === -90 ){  
+			options.orientationchange('landscape') 
+		} 	
     }, false);
 
 
